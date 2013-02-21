93d20ac87fb3bcdf299b94d8a411113ba0305587
knop <knop0211@gmail.com>
2013-02-21 11:52:24 +0800

    修改异常编码定义

--- src/com/team4/utils/exceptions/T4Code.java
+++ src/com/team4/utils/exceptions/T4Code.java
@@ -9,12 +9,15 @@ package com.team4.utils.exceptions;
  */
 
 public class T4Code {
+	//通用异常编码
 	public static final int UNKNOWN = -1;
-	
 	public static final int OK = 0;
+	public static final int NETWORK_ERROR = 99;
 	
-	public static final int NETWORK_ERROR = 100;
+	//标识APP中抛出的各种异常编码
+	public static final int APP_ERROR = 100;
 	
+	//标识解析时抛出的各种异常编码
 	public static final int PARSE_ERROR = 1000;
 }
 
